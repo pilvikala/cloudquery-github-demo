217b5a9ebfd8b9632eeafda930d01456aca9ab0a
pilvikala <michal@cloudquery.io>
2023-11-24 15:44:31 +0200

    fix view scripts

---
+++ typeform_view_cloud_responses.sql
@@ -0,0 +1,42 @@
+-- public.typeform_form_cloud_responses source
+
+CREATE OR REPLACE VIEW public.typeform_form_cloud_responses
+AS WITH names AS (
+         SELECT t1._cq_sync_time,
+            t1.submitted_at,
+            t1.response_id,
+            regexp_replace((t1.value -> 'text'::text)::text, '\"'::text, ''::text, 'g'::text) AS name
+           FROM ( SELECT tfr._cq_sync_time,
+                    tfr.submitted_at,
+                    tfr.response_id,
+                    jsonb_array_elements(tfr.answers) AS value
+                   FROM typeform_form_responses tfr
+                  WHERE tfr.form_id = 'MZ0DSN82'::text) t1
+          WHERE ((((t1.value -> 'field'::text) -> 'id'::text)::json)::text) = '"RJDdgkvvWaie"'::text
+        ), emails AS (
+         SELECT t1.response_id,
+            regexp_replace((t1.email -> 'email'::text)::text, '\"'::text, ''::text, 'g'::text) AS email
+           FROM ( SELECT tfr.response_id,
+                    jsonb_array_elements(tfr.answers) AS email
+                   FROM typeform_form_responses tfr
+                  WHERE tfr.form_id = 'MZ0DSN82'::text) t1
+          WHERE ((((t1.email -> 'field'::text) -> 'id'::text)::json)::text) = '"yX1K7oplSHVX"'::text
+        ), answers AS (
+         SELECT t1.response_id,
+            regexp_replace((t1.answer -> 'text'::text)::text, '\"'::text, ''::text, 'g'::text) AS answer
+           FROM ( SELECT tfr.response_id,
+                    jsonb_array_elements(tfr.answers) AS answer
+                   FROM typeform_form_responses tfr
+                  WHERE tfr.form_id = 'MZ0DSN82'::text) t1
+          WHERE ((((t1.answer -> 'field'::text) -> 'id'::text)::json)::text) = '"HUnsZKqVPDBH"'::text
+        )
+ SELECT names._cq_sync_time,
+    names.submitted_at,
+    names.response_id,
+    names.name,
+    emails.email,
+    answers.answer,
+    regexp_replace(emails.email, '.*\@'::text, ''::text) AS domain_name
+   FROM names
+     JOIN emails ON emails.response_id = names.response_id
+     JOIN answers ON answers.response_id = names.response_id;
